599b2a6285154e140f9a076635439a965933ddea
zanko khaledi <zanko.kh75@gmail.com>
2024-12-12 03:00:24 +0330

    HasRole.php was updated.

--- src/HasRole.php
+++ src/HasRole.php
@@ -3,31 +3,46 @@
 namespace App\Traits;
 
 use App\Models\Role;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 trait HasRole
 {
-    public function roles()
+    /**
+     * @return BelongsToMany
+     */
+    public function roles():BelongsToMany
     {
         return $this->belongsToMany(Role::class, 'role_user');
     }
 
+    /**
+     * @param string $role
+     * @return bool
+     */
     public function hasRole(string $role): bool
     {
         return $this->roles()->where('name', $role)->exists();
     }
 
+    /**
+     * @return array
+     */
     public function permissions(): array
     {
         return $this->roles()->get()->flatMap(function (Role $role) {
-            return $role->permissions()->pluck('name')->toArray();
-        });
+            return $role->permissions()->pluck('name');
+        })->all();
     }
 
+    /**
+     * @param string $permission
+     * @return bool
+     */
     public function hasPermission(string $permission): bool
     {
         $permissions = $this->roles()->get()->flatMap(function (Role $role) {
-            return $role->permissions()->pluck('name')->toArray();
-        });
+            return $role->permissions()->pluck('name');
+        })->all();
 
         return in_array($permission, $permissions);
     }
